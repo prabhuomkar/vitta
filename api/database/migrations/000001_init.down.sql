068333ec48967b089fec5bb5165f5ad6d6037984
Omkar Prabhu <prabhuomkar@pm.me>
2024-10-10 17:42:56 +0530

    Added database migrations

---
+++ api/database/migrations/000001_init.down.sql
@@ -0,0 +1,6 @@
+DROP table budgets;
+DROP table transactions;
+DROP table payees;
+DROP table categories;
+DROP table groups;
+DROP table accounts;
---
+++ api/database/migrations/000001_init.up.sql
@@ -0,0 +1,63 @@
+CREATE TABLE accounts (
+    id UUID PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    off_budget BOOLEAN NOT NULL,
+    category TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE groups (
+    id UUID PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+    id UUID PRIMARY KEY,
+    group_id UUID,
+    name TEXT,
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (group_id) REFERENCES groups(id),
+    UNIQUE (group_id, name)
+);
+
+CREATE TABLE payees (
+    id UUID PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transactions (
+    id UUID PRIMARY KEY,
+    account_id UUID,
+    category_id UUID,
+    payee_id UUID,
+    name TEXT,
+    credit DOUBLE PRECISION,
+    debit DOUBLE PRECISION,
+    notes TEXT,
+    cleared_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_id) REFERENCES accounts(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (payee_id) REFERENCES payees(id)
+);
+
+CREATE TABLE budgets (
+    id UUID PRIMARY KEY,
+    category_id UUID,
+    year INTEGER,
+    month INTEGER,
+    budgeted DOUBLE PRECISION,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    UNIQUE (year, month, category_id)
+);
