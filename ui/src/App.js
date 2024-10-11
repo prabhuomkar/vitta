80a40f184dc5aeda4c096987ddf3841290157c10
akshaypithadiya <akshay.pithadiya@gmail.com>
2024-10-11 20:11:17 +0530

    feat: theme setup and added login

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,16 @@
-import { Heading } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Heading>Vitta</Heading>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ ui/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+
+const Home = () => {
+  return <Heading>Vitta</Heading>;
+};
+
+export default Home;
