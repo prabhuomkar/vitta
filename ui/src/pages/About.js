e327ffc4152421a603333fc33817f3596ed57846
Akshay Pithadiya <akshay.pithadiya@gmail.com>
2024-10-12 19:27:18 +0530

    Vitta UI initlal commit and setup (#1)
    * Vitta UI initlal commit and setup
    
    * Updated developer guide for the UI
    
    * configured name & email
    
    * Add CI workflow for UI
    
    * feat: theme setup and added login
    
    * feat: Added Main Layout
    
    ---------
    
    Co-authored-by: Akshay Pithadiya <akshaypithadiya@Akshays-MacBook-Air.local>

---
+++ ui/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const About = () => {
+  return (
+    <Box as="main" w="100%" padding="1rem">
+      <Heading>About</Heading>
+    </Box>
+  );
+};
+
+export default About;
---
+++ ui/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const Contact = () => {
+  return (
+    <Box as="main" w="100%" padding="1rem">
+      <Heading>Contact</Heading>
+    </Box>
+  );
+};
+
+export default Contact;
